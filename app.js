63b35cb8961ba9cdea77dddb068029d537edc4a9
Bao Nguyen <duybaonguyen4@gmail.com>
2023-10-04 14:13:55 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const cors= require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const contactsRouter = require("./app/routes/contact.route");
-const ApiError = require("./app/api-error");
+const contactRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactsRouter);
+app.use('/api/contacts', contactRouter);
+
+
 
 app.get('/', (req, res) => {
     res.json({ message: " Welcome to contact book application." })
